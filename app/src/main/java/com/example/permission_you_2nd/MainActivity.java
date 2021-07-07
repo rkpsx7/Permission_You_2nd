135672ec60d79c8b78b35b80f18e2af97fa9c631
rkpsx7 <rkpsx7@gmail.com>
2021-07-07 13:45:17 +0530

    1st

---
+++ app/src/main/java/com/example/permission_you_2nd/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.permission_you_2nd;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
