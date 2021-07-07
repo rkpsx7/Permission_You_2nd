2ce01e1be24d367927c8712b3567f63f7a57df41
rkpsx7 <rkpsx7@gmail.com>
2021-07-07 22:23:17 +0530

    completed

--- app/src/main/java/com/example/permission_you_2nd/MainActivity.java
+++ app/src/main/java/com/example/permission_you_2nd/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.permission_you_2nd;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -29,6 +31,19 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED)
+            showToast("Storage and Location Permissions Granted");
+        else if (grantResults[0] == PackageManager.PERMISSION_DENIED && grantResults[1] == PackageManager.PERMISSION_GRANTED)
+            showToast("Storage Denied & Location Granted");
+        else if (grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_DENIED)
+            showToast("Storage Granted & Location Denied");
+        else if (grantResults[0] == PackageManager.PERMISSION_DENIED && grantResults[1] == PackageManager.PERMISSION_DENIED)
+            showToast("Both Permissions Denied");
+    }
+
     public void showToast(String msg){
         Toast.makeText(MainActivity.this,msg,Toast.LENGTH_SHORT).show();
     }
